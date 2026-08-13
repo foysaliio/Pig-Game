7104dc0fd4d805a9662505cd1d1b69d47c0dc87c
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-14 01:17:23 +0600

    feat: implement new game reset and finalize Pig Game

--- script.js
+++ script.js
@@ -13,15 +13,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const swithcPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -52,6 +65,7 @@ btnRoll.addEventListener('click', () => {
   }
 });
 
+// Button Hold functionality
 btnHold.addEventListener('click', () => {
   if (playing) {
     // 1. Add current score to active player's score
@@ -60,7 +74,7 @@ btnHold.addEventListener('click', () => {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -76,3 +90,6 @@ btnHold.addEventListener('click', () => {
     }
   }
 });
+
+// Button reset functionality
+btnNew.addEventListener('click', init);
